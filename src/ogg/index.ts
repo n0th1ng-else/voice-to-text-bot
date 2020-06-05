5e3bbd18a90665f4d04fac3922dd6df13be18f3d
Sergey Nikitin <srg.post@gmail.com>
2020-06-05 18:56:16 +0700

    fix(file-converter): Convert OGG to WAV (Closes #25)
    We now support Desktop clients and browser versions of Telegram

--- src/ogg/index.ts
+++ src/ogg/index.ts
@@ -1,41 +1,32 @@
 import generateHeader from "waveheader";
 import { opus } from "prism-media";
 import { get as runGet } from "https";
-import { createWriteStream } from "fs";
 import { Logger } from "../logger";
 
 const logger = new Logger("ogg-to-wav");
 
-export function getWav(fileLink: string) {
+export function getWav(fileLink: string): Promise<Buffer> {
   logger.info("Converting into wav 💿");
 
-  return new Promise((resolve, reject) => {
+  return new Promise<Buffer>((resolve, reject) => {
     runGet(fileLink, (response) => {
-      // Store original file into hard drive
-      const oggFile = createWriteStream("./audio.ogg");
-      response.pipe(oggFile);
-
-      // Store converted file into buffer
       const wavBuffer: Buffer[] = [];
+
+      // OggDemuxer converts into raw PCM data so we need to have a WAV header
+      const wavStream = response
+        .pipe(new opus.OggDemuxer())
+        .pipe(new opus.Decoder({ rate: 48000, channels: 1, frameSize: 960 }));
+
+      // WAV header here we go
       wavBuffer.push(
         generateHeader(0, {
           sampleRate: 48000,
         })
       );
 
-      const wavStream = response
-        .pipe(new opus.OggDemuxer())
-        .pipe(new opus.Decoder({ rate: 48000, channels: 1, frameSize: 960 }));
-
       wavStream.on("data", (bufferPart) => wavBuffer.push(bufferPart));
       wavStream.on("error", (err) => reject(err));
-      wavStream.on("end", () => {
-        // Store converted file into hard drive
-        const wavFile = createWriteStream("./audio.wav");
-        wavFile.write(Buffer.concat(wavBuffer));
-        //
-        resolve(Buffer.concat(wavBuffer));
-      });
-    });
+      wavStream.on("end", () => resolve(Buffer.concat(wavBuffer)));
+    }).on("error", (err) => reject(err));
   });
 }
--- src/recognition/types.ts
+++ src/recognition/types.ts
@@ -1,5 +1,3 @@
-import { IAudioMetadata } from "music-metadata/lib/type";
-
 export enum VoiceConverterProvider {
   Google = "GOOGLE",
   Aws = "AWS",
@@ -17,25 +15,6 @@ export interface VoiceConverterOptions {
   googleProjectId?: string;
   googleClientEmail?: string;
   googlePrivateKey?: string;
-}
-
-export class AudioFileData {
-  constructor(
-    private readonly buffer: Buffer,
-    private readonly meta: IAudioMetadata
-  ) {}
-
-  public get plainBuffer(): string {
-    return this.buffer.toString("base64");
-  }
-
-  public get sampleRate(): number | undefined {
-    return this.meta.format.sampleRate;
-  }
-
-  public get numberOfChannels(): number | undefined {
-    return this.meta.format.numberOfChannels;
-  }
 }
 
 export enum LanguageCode {
