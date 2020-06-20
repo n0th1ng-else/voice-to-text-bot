64ba35cb9bd89f5895405b183a27109da539b2a2
Sergey Nikitin <srg.post@gmail.com>
2020-06-20 17:18:31 +0700

    fix(): Refactor launcher script. Add some logging and checks

--- src/scripts/runJs.js
+++ src/scripts/runJs.js
@@ -1,3 +1,15 @@
-const scriptName = process.argv[process.argv.length - 1];
+const { existsSync } = require("fs");
+const { resolve } = require("path");
 
-require(`./${scriptName}.js`).run();
+const options = [...process.argv];
+const fileExt = options.pop();
+const fileName = options.pop();
+const file = `${fileName}.${fileExt}`;
+const scriptPath = fileExt && fileName && resolve(__dirname, file);
+
+if (scriptPath && existsSync(scriptPath)) {
+  const { run } = require(scriptPath);
+  run();
+} else {
+  throw new Error(`Unable to find script "${file}" in "${scriptPath}"`);
+}
--- src/scripts/runTs.js
+++ src/scripts/runTs.js
@@ -1,5 +1,2 @@
 require("ts-node").register();
-
-const scriptName = process.argv[process.argv.length - 1];
-
-require(`./${scriptName}.ts`).run();
+require("./runJs");
