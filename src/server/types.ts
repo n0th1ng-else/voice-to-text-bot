89bbbb0a80777fe829bbd5a36a3397c9a26bc270
Sergey Nikitin <srg.post@gmail.com>
2020-07-22 00:52:06 +0700

    feat(app-events): Implement lifecycle api hook (Closes #123)
    We can listen to the other replicas and run instance on node restart

--- src/server/types.ts
+++ src/server/types.ts
@@ -16,3 +16,35 @@ export enum HealthSsl {
   On = "ON",
   Off = "OFF",
 }
+
+export class HealthModel {
+  private readonly ssl: HealthSsl;
+  private status = HealthStatus.InProgress;
+  private message = "Waiting for bots to set up";
+  private urls: string[] = [];
+
+  constructor(private readonly version: string, isHttps: boolean) {
+    this.ssl = isHttps ? HealthSsl.On : HealthSsl.Off;
+  }
+
+  public setOnline(urls: string[]): void {
+    this.status = HealthStatus.Online;
+    this.message = "All is good";
+    this.urls = [...urls];
+  }
+
+  public setError(errMessage: string): void {
+    this.status = HealthStatus.Error;
+    this.message = errMessage;
+  }
+
+  public getDto(): HealthDto {
+    return {
+      ssl: this.ssl,
+      version: this.version,
+      status: this.status,
+      urls: this.urls,
+      message: this.message,
+    };
+  }
+}
