27f28d51cf304087426a5136fb62239641da3dbe
Sergey Nikitin <srg.post@gmail.com>
2022-10-08 20:29:23 +0200

    fix(): Revert cluster

--- src/scripts/cluster.ts
+++ src/scripts/cluster.ts
@@ -10,40 +10,35 @@ export const run = (): void => {
   const launchTime = new Date().getTime();
 
   getHostName(appPort, selfUrl, ngRokToken).then((host) => {
-    if (!cluster.isPrimary) {
-      return;
+    if (cluster.isMaster) {
+      const isCLusterSizeValid = clusterSize && clusterSize > 0;
+      if (!isCLusterSizeValid) {
+        logger.error(
+          `Cluster size is not valid. Falling back to size=1. cLusterSize=${clusterSize}`,
+          new Error("Cluster size is not valid")
+        );
+      }
+      const size = isCLusterSizeValid ? clusterSize : 1;
+
+      Array(size)
+        .fill(null)
+        .forEach(() =>
+          cluster.fork({
+            SELF_URL: host,
+            LAUNCH_TIME: launchTime,
+          })
+        );
+    } else {
+      // @ts-expect-error We are inside worker, worker is defind
+      runServer(cluster.worker.id);
     }
 
-    const isCLusterSizeValid = clusterSize && clusterSize > 0;
-    if (!isCLusterSizeValid) {
-      logger.error(
-        `Cluster size is not valid. Falling back to size=1. cLusterSize=${clusterSize}`,
-        new Error("Cluster size is not valid")
-      );
-    }
-    const size = isCLusterSizeValid ? clusterSize : 2;
-    const spawnWorker = () => {
-      const newWorker = cluster.fork({
+    cluster.on("exit", (worker) => {
+      logger.warn(`Thread ${worker.id} has died. Spawning a new one`);
+      cluster.fork({
         SELF_URL: host,
         LAUNCH_TIME: launchTime,
       });
-      runServer(newWorker.id);
-    };
-
-    Array(size)
-      .fill(null)
-      .forEach(() => spawnWorker());
-
-    const list = Object.keys(cluster.workers || {});
-    logger.info(
-      `Instance has spawned ${list.length} workers with IDs=${list.join(", ")}`
-    );
-
-    cluster.on("exit", (worker) => {
-      logger.warn(
-        `Thread ${worker.id} has died=${worker.isDead()}. Spawning a new one`
-      );
-      spawnWorker();
     });
   });
 };
