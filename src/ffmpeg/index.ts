195bc607e8a586cbc9aad9b31fde22fe3f6f49dd
Sergey Nikitin <srg.post@gmail.com>
2023-06-12 23:05:28 +0200

    fix(ffmpreg): Revert changes

--- src/ffmpeg/index.ts
+++ src/ffmpeg/index.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import prism from "prism-media";
 import { Logger } from "../logger/index.js";
 import { isDebug } from "../env.js";
-import { wavSampleRate } from "../const.js";
+import { wavSampleRate as sampleRate } from "../const.js";
 import {
   deleteFileIfExists,
   saveBufferToFile,
@@ -23,59 +23,22 @@ const downloadAsStream = (url: string): Promise<IncomingMessage> =>
     })
     .then(({ data: stream }) => stream);
 
-const getMpegDecoderArgs = (fileName?: string): string[] => {
-  const input = fileName ? ["-i", fileName] : [];
-  return [
-    ...input,
-    "-analyzeduration",
-    "0",
-    "-loglevel",
-    "0",
-    "-f",
-    "s16le",
-    "-ar",
-    String(wavSampleRate),
-    "-ac",
-    "1",
-  ];
-};
-
-const getMpegDecoderStream = (fileName?: string): prism.FFmpeg =>
-  new FFmpeg({
-    args: getMpegDecoderArgs(fileName),
-  });
-
-const readWavBuffer = (wavStream: prism.FFmpeg): Promise<Buffer> => {
-  return new Promise<Buffer>((resolve, reject) => {
-    const wavBuffer: Buffer[] = [];
-    wavStream.on("data", (bufferPart) => wavBuffer.push(bufferPart));
-    wavStream.on("error", (err) => reject(err));
-    wavStream.on("end", () => resolve(Buffer.concat(wavBuffer)));
-  });
-};
-
-const readWavBufferFromFile = (fileName: string): Promise<Buffer> => {
-  const wavStream = getMpegDecoderStream(fileName);
-  return readWavBuffer(wavStream);
-};
-
-const readWavBufferFromStream = (stream: IncomingMessage): Promise<Buffer> => {
-  const wavStream = stream.pipe(getMpegDecoderStream());
-  return readWavBuffer(wavStream);
-};
-
 const getWavBufferFromAudio = (fileLink: string): Promise<Buffer> => {
   logger.info("Converting 🎶 AUDIO into wav 💿");
 
   return downloadAsStream(fileLink).then((stream) =>
-    // TODO refactor nested chain
-    readWavBufferFromStream(stream).then((buff) => {
+    new Promise<Buffer>((resolve, reject) => {
+      const wavBuffer: Buffer[] = [];
+      const wavStream = stream.pipe(getMpegDecoderStream());
+      wavStream.on("data", (bufferPart) => wavBuffer.push(bufferPart));
+      wavStream.on("error", (err) => reject(err));
+      wavStream.on("end", () => resolve(Buffer.concat(wavBuffer)));
+    }).then((buff) => {
       if (isDebug) {
         logger.info(`The link is ${fileLink}`);
-        return saveBufferToFile(buff, "", "output.wav").then((fileName) => {
-          logger.info(`The file is ${fileName}`);
-          return buff;
-        });
+        saveBufferToFile(buff, "", "output.wav").then((fileName) =>
+          logger.info(`The file is ${fileName}`)
+        );
       }
 
       return buff;
@@ -89,18 +52,44 @@ const getWavBufferFromVideo = (fileLink: string): Promise<Buffer> => {
   return downloadAsStream(fileLink)
     .then((stream) => saveStreamToFile(stream, "video-temp"))
     .then((fileName) =>
-      readWavBufferFromFile(fileName).finally(() => {
+      new Promise<Buffer>((resolve, reject) => {
+        const wavBuffer: Buffer[] = [];
+        const wavStream = getMpegDecoderStream(fileName);
+        wavStream.on("data", (bufferPart) => wavBuffer.push(bufferPart));
+        wavStream.on("error", (err) => reject(err));
+        wavStream.on("end", () => resolve(Buffer.concat(wavBuffer)));
+      }).finally(() => {
         if (isDebug) {
           logger.info(`The link is ${fileLink}`);
           logger.info(`The file is ${fileName}`);
           return;
         }
-
-        // TODO refactor nested chain and .finally: does not accept Promise as a return value
         return deleteFileIfExists(fileName);
       })
     );
 };
+
+const getMpegDecoderArgs = (fileName?: string): string[] => {
+  const input = fileName ? ["-i", fileName] : [];
+  return [
+    ...input,
+    "-analyzeduration",
+    "0",
+    "-loglevel",
+    "0",
+    "-f",
+    "s16le",
+    "-ar",
+    String(sampleRate),
+    "-ac",
+    "1",
+  ];
+};
+
+const getMpegDecoderStream = (fileName?: string): prism.FFmpeg =>
+  new FFmpeg({
+    args: getMpegDecoderArgs(fileName),
+  });
 
 export const getWav = (fileLink: string, isVideo: boolean): Promise<Buffer> =>
   isVideo ? getWavBufferFromVideo(fileLink) : getWavBufferFromAudio(fileLink);
