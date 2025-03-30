75e4f0c48e1158cd19d5ecbd5af6ca3177d777d6
Sergey Nikitin <srg.post@gmail.com>
2025-03-31 01:03:17 +0200

    fix(telegram): Do not initialize api module inside the Reflector (#458)
    do not initialize the Telegram api module twice. because it is an
    implicit dependency for the Error Reflector. In fact. we better off
    to provide specific api into error reflector and then use in the
    api module.

--- src/telegram/api/tgapi.ts
+++ src/telegram/api/tgapi.ts
@@ -7,14 +7,20 @@ import { TelegramChatsApi } from "./groups/chats/chats-api.js";
 export const TELEGRAM_API_MAX_MESSAGE_SIZE = 4096;
 
 export class TelegramApi {
+  private readonly baseClient: TelegramBaseApi;
+
   public readonly chats: TelegramChatsApi;
   public readonly updates: TelegramUpdatesApi;
   public readonly payments: TelegramPaymentsApi;
 
-  constructor(apiToken: string, errorReflector?: ApiErrorReflector) {
-    const client = new TelegramBaseApi(apiToken, errorReflector);
-    this.chats = new TelegramChatsApi(client, apiToken);
-    this.updates = new TelegramUpdatesApi(client);
-    this.payments = new TelegramPaymentsApi(client);
+  constructor(apiToken: string) {
+    this.baseClient = new TelegramBaseApi(apiToken);
+    this.chats = new TelegramChatsApi(this.baseClient, apiToken);
+    this.updates = new TelegramUpdatesApi(this.baseClient);
+    this.payments = new TelegramPaymentsApi(this.baseClient);
+  }
+
+  public setErrorReflector(errorReflector: ApiErrorReflector): void {
+    this.baseClient.setErrorReflector(errorReflector);
   }
 }
