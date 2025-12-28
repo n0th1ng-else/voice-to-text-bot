d8eec84b3a435ade0d2d7b4de696e4b54ab09c62
Sergey Nikitin <srg.post@gmail.com>
2025-12-28 21:14:35 +0100

    feat(engine): Experiment with Bun engine

---
+++ src/monitoring/sentry/sentry-bun.ts
@@ -0,0 +1,77 @@
+import {
+  init as initSentryGlobal,
+  captureException,
+  captureMessage,
+  getCurrentScope,
+  withIsolationScope,
+  setupFastifyErrorHandler,
+} from "@sentry/bun";
+import type { FastifyInstance } from "fastify";
+import { SentryBase, type SentryMetadata } from "./sentry-base.js";
+import { isDevelopment } from "../../common/environment.js";
+import type { VoidFunction } from "../../common/types.js";
+
+export class SentryBunClient extends SentryBase {
+  public init(app: FastifyInstance): void {
+    if (!this.isEnabled()) {
+      return;
+    }
+
+    initSentryGlobal({
+      dsn: this.sentryDsn,
+      environment: this.nodeEnvironment,
+      release: this.appVersion,
+
+      // Set tracesSampleRate to 1.0 to capture 100%
+      // of transactions for performance monitoring.
+      // We recommend adjusting this value in production
+      tracesSampleRate: isDevelopment() ? 1.0 : 0.05,
+      sampleRate: isDevelopment() ? 1.0 : 0.5,
+      beforeSend: (event, hint) => this.beforeSentrySend(event, hint),
+    });
+
+    this.setFastifyRequestPlugin(app);
+    setupFastifyErrorHandler(app);
+  }
+
+  public captureException(error: unknown): void {
+    if (!this.isEnabled()) {
+      return;
+    }
+
+    captureException(error);
+  }
+
+  public captureMessage(
+    message: string,
+    context?: { level: "warning"; extra?: Record<string, unknown> },
+  ): void {
+    if (!this.isEnabled()) {
+      return;
+    }
+    captureMessage(message, context);
+  }
+
+  public addAttachment(filename: string, data: string | Uint8Array): void {
+    getCurrentScope().addAttachment({
+      filename,
+      data,
+      contentType: "text/plain",
+    });
+  }
+
+  public clearAttachments(): void {
+    getCurrentScope().clearAttachments();
+  }
+
+  public setMetadataAndTags(
+    meta: SentryMetadata,
+    tags: Record<string, string>,
+    doneFn: VoidFunction,
+  ): void {
+    withIsolationScope(() => {
+      getCurrentScope().setSDKProcessingMetadata(meta).setTags(tags);
+      doneFn();
+    });
+  }
+}
