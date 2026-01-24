319430f750d30b01418d5be0a3b1235bc9642f13
Sergey Nikitin <srg.post@gmail.com>
2026-01-25 00:52:03 +0100

    chore(): Add new commits types

--- release.config.js
+++ release.config.js
@@ -1,6 +1,15 @@
 export default {
   plugins: [
-    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "angular",
+        releaseRules: [
+          { type: "refactor", release: "patch" },
+          { type: "chore", release: "patch" },
+        ],
+      },
+    ],
     "@semantic-release/release-notes-generator",
     "@semantic-release/github",
     "./release-exec.js",
