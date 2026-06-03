18387a4951bdfed42f9ff77655cc7bf219853f29
Sergey Nikitin <srg.post@gmail.com>
2026-06-03 23:47:42 +0200

    fix(memory): openAsBlob is holding the memory
    openAsBlob does not always release the file handler, hence the Buffer stays in memory. We are attempting to replace it with readFile - so read the whole file into
    memory, but then release the whole thing and this will allow us to keep stable MEM consumption

--- src/ffmpeg/index.ts
+++ src/ffmpeg/index.ts
@@ -4,7 +4,7 @@ import ffmpeg from "fluent-ffmpeg";
 import { Logger } from "../logger/index.js";
 import { deleteFileIfExists, saveStreamToFile } from "../files/index.js";
 import { API_TIMEOUT_MS, wavSampleRate } from "../const.js";
-import { openAsBlob } from "node:fs";
+import { readFile } from "node:fs/promises";
 import { getResponseErrorData } from "../server/error.js";
 
 const logger = new Logger("media-to-wav");
@@ -100,6 +100,7 @@ export const getAudioBlob = async (
   shouldConvertToWav = true,
 ): Promise<[Blob, string]> => {
   const filePath = await getAudioFilePath(fileLink, isLocalFile, shouldConvertToWav);
-  const fileBlob = await openAsBlob(filePath);
+  const fileBuffer = await readFile(filePath);
+  const fileBlob = new Blob([fileBuffer]);
   return [fileBlob, filePath];
 };
